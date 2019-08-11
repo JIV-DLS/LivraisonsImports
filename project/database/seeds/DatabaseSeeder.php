3f7395745c0792a772f4b0e0b00392945d5f09e5
jiv <aniglojonas@gmail.com>
2019-08-11 23:02:20 +0000

    aSeedWithoutUserSeeder

--- project/database/seeds/DatabaseSeeder.php
+++ project/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
         $this->call(BikesTableSeeder::class);
         $this->call(BuildersTableSeeder::class);
         $this->call(ItemsTableSeeder::class);
